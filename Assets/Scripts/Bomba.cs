85b12abcfbe8bfddb8d9430a7ea3916b0a730f32
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-05-17 20:04:01 +0300

    change bomb code

--- Assets/Scripts/Bomba.cs
+++ Assets/Scripts/Bomba.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Bomba : MonoBehaviour
 {
     public GameObject explosionPrefab;
     public GameObject panelGameOver;
+    public UnityEvent blowUp;
+
 
     private void OnTriggerEnter(Collider other)
     {
           if (other.CompareTag("Player"))
         {
             Instantiate(explosionPrefab, transform.position, transform.rotation);
-            Destroy(explosionPrefab);
-            Time.timeScale = 1f;
+            blowUp.Invoke();
             panelGameOver.SetActive(true);
         }
     }
