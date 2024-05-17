ffcd81bb945f23576b7ba9c2eadf6efb32501188
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-05-17 16:01:13 +0300

    Delete unnecessary scene and add sound effects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 5;
     [SerializeField] private CharacterController _characterController;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public UnityEvent addCoin;
+    public UnityEvent collisionBomb;
 
-    // Update is called once per frame
     void Update()
     {
         _characterController.Move(Vector3.right * _speed * Input.GetAxis("Horizontal") * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "coin")
+        {
+            addCoin.Invoke();
+        }
+        else if (other.tag == "bomb")
+        {
+            collisionBomb.Invoke();
+        }
     }
 }
