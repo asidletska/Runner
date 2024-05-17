9f3fcf15377807eb25fd218a162bfa4daad22b19
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-05-17 19:43:53 +0300

    Add animation bomb

--- Assets/Scripts/Bomba.cs
+++ Assets/Scripts/Bomba.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bomba : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject explosionPrefab;
+    public GameObject panelGameOver;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+          if (other.CompareTag("Player"))
+        {
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(explosionPrefab);
+            Time.timeScale = 1f;
+            panelGameOver.SetActive(true);
+        }
     }
 }
